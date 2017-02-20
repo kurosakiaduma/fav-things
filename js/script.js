c7132107325945465e01b88b24c911aac354ae41
Tevin Aduma <Tevin Aduma>
2017-02-20 20:27:31 +0300

    add variables, array and unordered lisr

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-    
+
+    $("#blanks form").submit(function(event) {
+        var name = document.getElementById("name").value;
+        var game = document.getElementById("game").value;
+        var animal = document.getElementById("animal").value;
+        var sport = document.getElementById("sport").value;
+        var book = document.getElementById("book").value;
+
+
+        var showMe = [];
+        showMe.push(game, name, animal);
+
+         $('#showAll').append("<li>"+game+"</li>", "<li>"+name+"</li>","<li>"+animal+"</li>");
+
+        $("#favorite-things").show();
+
+        event.preventDefault()
+    });
 });
